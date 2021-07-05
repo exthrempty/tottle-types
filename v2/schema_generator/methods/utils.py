cf7cd74a7668ed46c09abc1a9c46ee65237044b1
exthrempty <andrewsalt.e@gmail.com>
2021-07-05 23:37:56 +0500

    Delete unnecessary utilities

--- v2/schema_generator/methods/utils.py
+++
@@ -1,8 +0,0 @@
-def get_set_params(params: dict) -> dict:
-    return {
-        k if not k.endswith("_") else k[:-1]: v
-        for k, v in params.items()
-        if k != "self" and v is not None
-    }
-
-
