dd37a5059d5905b89e846dbdddc1cc8865c6d905
geo_madness <andrewsalt.e@gmail.com>
2021-07-28 23:27:32 +0500

    commit with lots & lots of unfinished work

--- v2/schema_generator/utils/imports.py
+++ v2/schema_generator/utils/imports.py
@@ -3,7 +3,7 @@ from subprocess import check_output
 from typing import Tuple
 
 
-def format_file(path: Path) -> Tuple[bytes, bytes]:
+def sort_imports(line: str) -> str:
     black = check_output(["black", "-q", path])
     isort = check_output(["isort", path])
     return black, isort
